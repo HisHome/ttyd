9dca93bdaedd3cdaf2761240c3982f67c93abc96
pengyang <pengyang@huitongjy.com>
2019-05-25 15:51:07 +0800

    微信

--- app.js
+++ app.js
@@ -1,32 +1,23 @@
-var express = require('express'); //express 框架 
-var crypto = require('crypto');  //引入加密模块
-var config = require('./config');//引入配置文件
+const express = require('express'), //express 框架 
+      wechat  = require('./wechat/wechat'), 
+      config = require('./config');//引入配置文件
 
 //实例 express
 var app = express();
-//用于处理所有进入 3080 端口 get 的连接请求
+
+var wechatApp = new wechat(config); //实例wechat 模块
+
+//用于处理所有进入 3200 端口 get 的连接请求
 app.get('/', function (req, res) {
-    //1.获取微信服务器Get请求的参数 signature、timestamp、nonce、echostr
-    var signature = req.query.signature,//微信加密签名
-        timestamp = req.query.timestamp,//时间戳
-        nonce = req.query.nonce,//随机数
-        echostr = req.query.echostr;//随机字符串
-
-    //2.将token、timestamp、nonce三个参数进行字典序排序
-    var array = [config.token, timestamp, nonce];
-    array.sort();
-
-    //3.将三个参数字符串拼接成一个字符串进行sha1加密
-    var tempStr = array.join('');
-    const hashCode = crypto.createHash('sha1'); //创建加密类型 
-    var resultCode = hashCode.update(tempStr, 'utf8').digest('hex'); //对传入的字符串进行加密
-
-    //4.开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
-    if (resultCode === signature) {
-        res.send(echostr);
-    } else {
-        res.send('mismatch');
-    }
+    wechatApp.auth(req,res);
 });
-//监听3000端口
+
+//用于请求获取 access_token
+app.get('/getAccessToken',function(req,res){
+    wechatApp.getAccessToken().then(function(data){
+        res.send(data);
+    });    
+});
+
+//监听3200端口
 app.listen(3200);
