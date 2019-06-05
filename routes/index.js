ff0b04e327221a8d5aabd09cf2b9327e1543dcfd
pengyang <pengyang@huitongjy.com>
2019-06-05 10:14:35 +0800

    微信

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
-var request = require('../tool/request.js');
-var logger = require("../tool/logger");
 
 
 router.get('/', function(req,res){
