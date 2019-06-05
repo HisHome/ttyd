bd9d213922789c39771c200620f60b3246aa709f
pengyang <pengyang@huitongjy.com>
2019-06-05 10:21:30 +0800

    微信

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express'), //express 框架
       wechat  = require('./wechat/wechat'), 
        config = require('./config');//引入配置文件
        
-var routes = require('./routes/index');
-var path = require('path');
-
 
 var app = express();//实例express框架
 
@@ -14,9 +11,6 @@ var wechatApp = new wechat(config); //实例wechat 模块
 app.get('/',function(req,res){
     wechatApp.auth(req,res);
 });
-
-app.use("/",express.static(path.join(__dirname, 'views'),{index:false}));
-app.get('/pages', routes);
 
 
 //用于处理所有进入 3080 端口 post 的连接请求
